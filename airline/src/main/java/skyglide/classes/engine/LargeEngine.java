2324f0ffc4a967a577c898736ff6f81f15675077
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2024-05-08 22:55:09 +0300

    added classes and database

---
+++ airline/src/main/java/skyglide/classes/engine/LargeEngine.java
@@ -0,0 +1,11 @@
+package skyglide.classes.engine;
+
+public class LargeEngine extends Engine {
+    LargeEngine(){
+        this.speed = 60;
+    }
+    
+    public void turnoff(){
+        System.out.println("it's off now");
+    }
+}

a0297b04353f112f094b5c78fdd912f7287ef706
Youssef Essa <Youssef.Essa0gmail.com>
2024-05-11 10:39:33 +0300

    New Files Added By Jo Essa

---
+++ airline/src/main/java/skyglide/Java_App_OOP/Collecting_Data.java
@@ -0,0 +1,20 @@
+package skyglide.Java_App_OOP;
+
+public abstract class Collecting_Data {
+
+    /*
+
+      ABSTRACT CLASS :
+
+      1 : important To Collect All Our Projects' Methods 
+      2 : Doesn't Contain Any Body 
+      3 : DON'T CHANGE As It Remains Fixed To All Only Adding Methods
+      4 : Acts As =====>   DOCUMENTARY   <===== 
+
+    */
+
+    abstract public void Show_Data ();
+    abstract public void Data_Request();
+    
+    
+}

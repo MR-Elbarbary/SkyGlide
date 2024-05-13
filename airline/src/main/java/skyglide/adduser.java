1893ff750ac210c24f6079f3711337154c30d7d3
Youssef Essa <Youssef.Essa0gmail.com>
2024-05-13 23:13:26 +0300

    New Modifications

--- airline/src/main/java/skyglide/adduser.java
+++ airline/src/main/java/skyglide/adduser.java
@@ -2,24 +2,41 @@ package skyglide;
 
 import java.io.IOError;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 import skyglide.classes.DatabaseConnection;
 
-public class adduser {
+public class adduser implements Initializable {
+
+    @FXML
+    private Label AddUser;
+
+    @FXML
+    private Pane Moving_Pane;
+
+    @FXML
+    private AnchorPane Main_Pane;
 
     @FXML
     private Button save;
 
     @FXML
-    private TextField username;
+    private TextField useremail;
 
     @FXML
-    private TextField useremail;
+    private TextField username;
 
     @FXML
     private TextField userpassword;
@@ -48,6 +65,22 @@ public class adduser {
         username.setText(null);
         userpassword.setText(null);
         useremail.setText(null);
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(AddUser);
+        translate.setDuration(Duration.millis(1100));
+        translate.setByX(200);
+        translate.play();
+
+        TranslateTransition translate1 = new TranslateTransition();
+        translate1.setNode(Moving_Pane);
+        translate1.setDuration(Duration.millis(1500));
+        translate1.setByX(200);
+        translate1.play();
     }
 
 }

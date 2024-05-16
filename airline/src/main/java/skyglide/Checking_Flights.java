7bc6918d6e5c4a728628c3c3cf20db4f2a6ca66f
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2024-05-16 21:34:40 +0300

    back fixed and added the new report

--- airline/src/main/java/skyglide/Checking_Flights.java
+++ airline/src/main/java/skyglide/Checking_Flights.java
@@ -86,7 +86,7 @@ public class Checking_Flights implements Initializable {
     @FXML
     void Back(ActionEvent event) throws IOException{
 
-        Parent root = FXMLLoader.load(getClass().getResource("AdminPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
--- airline/src/main/java/skyglide/Report.java
+++ airline/src/main/java/skyglide/Report.java
@@ -3,7 +3,6 @@ package skyglide;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,73 +13,74 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class Report implements Initializable{
-
-   
+public class Report implements Initializable {
     @FXML
     private Button Back;
-
+    @FXML
+    private Button submit;
     @FXML
     private Label Label;
-
     @FXML
     private Label Label2;
-
+    @FXML
+    private Label Label3;
     @FXML
     private Pane Left_Pane;
-
     @FXML
     private Pane Moving_Line;
-
     @FXML
     private Pane Small_Pane;
+    @FXML
+    private TextArea TA;
 
+    public Report() {}
+    
 
     @FXML
-    void Back(ActionEvent event) throws IOException{
-
-         Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-         Scene scene = new Scene(root);
-         
-         stage.setTitle("SkgyGlide");
-         stage.setX(200);
-         stage.setY(5);
-         stage.setScene(scene);
-         stage.show();
-
+    void Back(ActionEvent event) throws IOException {
+        Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("HomePage.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setTitle("SkyGlide");
+        stage.setX(200.0);
+        stage.setY(5.0);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-
-        TranslateTransition translate = new TranslateTransition();
-        translate.setNode(Left_Pane);
-        translate.setDuration(Duration.millis(1000));
-        translate.setByX(190);
-        translate.play();        
-
-        TranslateTransition translate1 = new TranslateTransition();
-        translate1.setNode(Label);
-        translate1.setDuration(Duration.millis(1500));
-        translate1.setByY(80);
-        translate1.play();        
-        
-        TranslateTransition translate2 = new TranslateTransition();
-        translate2.setNode(Moving_Line);
-        translate2.setDuration(Duration.millis(1000));
-        translate2.setByY(85);
-        translate2.play();        
-
-        TranslateTransition translate3 = new TranslateTransition();
-        translate3.setNode(Small_Pane);
-        translate3.setDuration(Duration.millis(1400));
-        translate3.setByX(380);
-        translate3.play();        
-    }
-
+        try {
+            TranslateTransition translate = new TranslateTransition();
+            translate.setNode(this.Left_Pane);
+            translate.setDuration(Duration.millis(1000.0));
+            translate.setByX(186.0);
+            translate.play();
+    
+            TranslateTransition translate1 = new TranslateTransition();
+            translate1.setNode(this.Label);
+            translate1.setDuration(Duration.millis(1500.0));
+            translate1.setByY(110.0);
+            translate1.play();
+    
+            TranslateTransition translate2 = new TranslateTransition();
+            translate2.setNode(this.Moving_Line);
+            translate2.setDuration(Duration.millis(1000.0));
+            translate2.setByY(85.0);
+            translate2.play();
+    
+            TranslateTransition translate3 = new TranslateTransition();
+            translate3.setNode(this.Small_Pane);
+            translate3.setDuration(Duration.millis(1400.0));
+            translate3.setByX(379.0);
+            translate3.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle the exception as needed
+        }
+}
 }
